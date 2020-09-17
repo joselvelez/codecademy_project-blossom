ccc6299a453582ad77b74cc00b1bd52dd1f1b143
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-09-17 17:33:20 -0500

    completed linked_list library

--- linked_list.py
+++ linked_list.py
@@ -13,43 +13,52 @@ class Node:
         self.next_node = value
 
 class LinkedList:
-    def __init__(self, head=None):
-        self.head = head
-        self.linked_list = []
+    def __init__(self, value=None):
+        self.value = value
+        self.head = None
 
-    def add_node(self, value):
-        next_node = self.head
-        node = Node(value, next_node)
-        self.head = node
-        self.linked_list.append(node)
+    def add_beginning(self, value):
+        new_node = Node(value)
+        new_node.set_next_node(self.head)
+        self.head = new_node
 
-    def remove_node(self, node):
-        pass
+    def remove_node(self, value_to_remove):
+        current_node = self.head
+        if current_node.get_value() == value_to_remove:
+            self.head = current_node.get_next_node()
+        else:
+            while current_node:
+                if current_node.get_next_node().get_value() == value_to_remove:
+                    current_node = current_node.set_next_node(current_node.get_next_node().get_next_node())
+                else:
+                    current_node = current_node.get_next_node()
 
     def get_head(self):
         return self.head
 
     def print_list(self):
-        lst = []
-        current_node = self.head
-        lst.append(current_node.get_value())
-        while current_node.get_next_node():
-            lst.append(current_node.get_next_node().get_value())
+        str_lst = ""
+        current_node = self.get_head()
+        while current_node:
+            if current_node.get_value() != None:
+                str_lst += str(current_node.get_value()) + " --> "
             current_node = current_node.get_next_node()
-        print(lst)
+        print(str_lst)
 
 ''' Test / Debug '''
 
-test_node = Node('test1')
-print("Node Value: {node}".format(node=test_node.get_value()))
-print("Next Node: {next}".format(next=test_node.get_next_node()))
+# test_node = Node('test1')
+# print("Node Value: {node}".format(node=test_node.get_value()))
+# print("Next Node: {next}".format(next=test_node.get_next_node()))
 
 ll = LinkedList()
-ll.add_node(1)
-ll.add_node("two")
-ll.add_node(3)
-ll.add_node("four")
-ll.add_node(5)
-ll.add_node("six")
+ll.add_beginning(1)
+ll.add_beginning("two")
+ll.add_beginning(3)
+ll.add_beginning("four")
+ll.add_beginning(5)
+ll.add_beginning("six")
+ll.add_beginning(7)
+ll.remove_node(3)
 print("Current Head Node: {head}".format(head=ll.get_head().get_value()))
 ll.print_list()
